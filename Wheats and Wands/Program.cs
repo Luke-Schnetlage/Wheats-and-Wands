123ce6823d8326dd09f05f08b8c925f2f47bf3e4
Luke Schnetlage <lschnetlage@my.apsu.edu>
2021-02-19 16:46:32 -0600

    MonoGame Framework

---
+++ Wheats and Wands/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Wheats_and_Wands
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
