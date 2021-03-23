1317b8f3ec22674efe538195029bb004c340e6c0
scott <scott@LAPTOP-UIN9PK12>
2021-03-22 20:50:48 -0500

    Added credits

---
+++ Wheats and Wands/Levels/CreditScreen.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wheats_and_Wands.Levels
+{
+    public class CreditScreen
+    {
+        Texture2D creditScreen { get; set; }
+
+        SpriteFont font { get; set; }
+
+        public CreditScreen(GameTime gameTime, Texture2D creditsScreen)
+        {
+
+        }
+    }
+}
