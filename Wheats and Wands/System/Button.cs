f3bfcd0683d56c59a74e11c9a4c94b3406260ca1
Luke Schnetlage <lschnetlage@my.apsu.edu>
2021-03-25 21:44:56 -0600

    Implimented Title screen buttons

--- Wheats and Wands/System/Button.cs
+++ Wheats and Wands/System/Button.cs
@@ -13,33 +13,42 @@ namespace Wheats_and_Wands.System
         private MouseState _currentMouse;
         private bool isHovering;
         private MouseState _previousMouse; //Checks left clicking?
-        private Texture2D _texture;
-
+        //private Texture2D _texture;
+        private Sprite _sprite;
         public event EventHandler Click;
+        public float X;
+        public float Y;
 
         public bool Clicked { get; private set; }
         public Color PenColor { get; set; } //Color of the font
-        public Vector2 Position { get; set; } //Position of the button
+        //public Vector2 Position { get; set; } //Position of the button
         public string Text { get; set; }
         public Rectangle Rectangle //Size of the button
         {
-            get
+           get
             {
-                return new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height);
+                return new Rectangle(_sprite.X , _sprite.Y - 540, _sprite.Width, _sprite.Height);
             }
         }
 
-        public Button(Texture2D texture)
+        public Button(Sprite sprite, float x, float y)
         {
-            _texture = texture;            
+            _sprite = sprite;
+            X = x;
+            Y = y;
+            //Position = new Vector2((float)_sprite.X, (float)_sprite.Y);
         }
         public override void Draw(SpriteBatch spriteBatch)
         {
-            var color = Color.White;
-            if (isHovering) //If the mouse is hovering over the button
-                color = Color.Gray;
+            //Color color = Color.White;
+            if (this.isHovering) //If the mouse is hovering over the button
+                _sprite.TintColor = Color.Gray;
+            else
+            {
+                _sprite.TintColor = Color.White;
+            }
 
-            spriteBatch.Draw(_texture, Rectangle, color);
+            _sprite.Draw(spriteBatch, new Vector2(_sprite.X, _sprite.Y - 540));
         }
 
         public override void Update(GameTime gameTime)
