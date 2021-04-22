958dc52c9304b253be2c952175815341cc8d59d2
scott <scott@LAPTOP-UIN9PK12>
2021-04-21 23:37:44 -0500

    Made space level easier

--- Wheats and Wands/Levels/Space.cs
+++ Wheats and Wands/Levels/Space.cs
@@ -36,9 +36,7 @@ namespace Wheats_and_Wands.Levels
             rand = new Random();
 
             _asteroid1 = new FallingKillObject(new Sprite(asteroid, 0, 0, 100, 115, new Vector2(270, -120)), _farmer, new TimeSpan(0, 0, rand.Next(1, 1)));
-            _asteroid2 = new FallingKillObject(new Sprite(asteroid, 0, 0, 100, 115, new Vector2(350, -120)), _farmer, new TimeSpan(0, 0, rand.Next(1, 4)));
             _asteroid3 = new FallingKillObject(new Sprite(asteroid, 0, 0, 100, 115, new Vector2(480, -120)), _farmer, new TimeSpan(0, 0, rand.Next(2, 3)));
-            _asteroid4 = new FallingKillObject(new Sprite(asteroid, 0, 0, 100, 115, new Vector2(600, -120)), _farmer, new TimeSpan(0, 0, rand.Next(1, 1)));
             _asteroid5 = new FallingKillObject(new Sprite(asteroid, 0, 0, 100, 115, new Vector2(640, -120)), _farmer, new TimeSpan(0, 0, rand.Next(5, 8)));
 
             _a1 = new HorizontalKillObject(new Sprite(asteroid, 0, 0, 100, 115, new Vector2(1000, 150)), _farmer, new TimeSpan(0, 0, rand.Next(1, 1)));
@@ -66,9 +64,7 @@ namespace Wheats_and_Wands.Levels
             }
 
             _asteroid1.Update(gameTime);
-            _asteroid2.Update(gameTime);
             _asteroid3.Update(gameTime);
-            _asteroid4.Update(gameTime);
             _asteroid5.Update(gameTime);
 
             _a1.Update(gameTime);
@@ -82,9 +78,7 @@ namespace Wheats_and_Wands.Levels
             _farmer.Draw(spriteBatch, gameTime);
 
             _asteroid1.Draw(spriteBatch, gameTime);
-            _asteroid2.Draw(spriteBatch, gameTime);
             _asteroid3.Draw(spriteBatch, gameTime);
-            _asteroid4.Draw(spriteBatch, gameTime);
             _asteroid5.Draw(spriteBatch, gameTime);
 
             _a1.Draw(spriteBatch, gameTime);
